426ce7ed71aea2fe1245c849595e892309083aa6
bahstiien <bastien.lecalvez@gmail.com>
2021-11-30 15:12:46 +0100

    resolve step2

--- pages/index.js
+++ pages/index.js
@@ -1,12 +1,16 @@
 import Head from "next/head";
 import Image from "next/image";
+import ImageBg from "../public/images/macbook.jpg";
 import styles from "../styles/Home.module.css";
 
 export default function Home() {
   return (
     <div>
-      <h1> Bienvenue sur mon portfolio</h1>
-      <p>
+      <h1 className="text-center text-gray-600 text-2xl p-10">
+        Bienvenue sur mon portfolio
+      </h1>
+      <Image src={ImageBg} alt="My desk" layout="responsive" />
+      <p className="text-center text-gray-600 text-lg p-10">
         Logoden biniou degemer mat an penn ar bed, digalon ne levrioù ennon
         anzav eta, mestr bed fresk gwinizh kerzu dreist.
       </p>
