887b428cd5fc9adb1ae6b447c1847f7391379416
bahstiien <bastien.lecalvez@gmail.com>
2021-11-30 15:23:55 +0100

    resolve step3

--- pages/index.js
+++ pages/index.js
@@ -2,10 +2,14 @@ import Head from "next/head";
 import Image from "next/image";
 import ImageBg from "../public/images/macbook.jpg";
 import styles from "../styles/Home.module.css";
+import Link from "next/link";
 
 export default function Home() {
   return (
     <div>
+      <div className="text-gray-500 text-lg pl-4 pt-6">
+        <Link href="/project"> Mes réalisations </Link>
+      </div>
       <h1 className="text-center text-gray-600 text-2xl p-10">
         Bienvenue sur mon portfolio
       </h1>
