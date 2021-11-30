e989d4d55a978bf8f490be976b8587bc9c736a80
bahstiien <bastien.lecalvez@gmail.com>
2021-11-30 14:42:18 +0100

    resolve step1

--- pages/index.js
+++ pages/index.js
@@ -1,9 +1,15 @@
-import Head from 'next/head'
-import Image from 'next/image'
-import styles from '../styles/Home.module.css'
+import Head from "next/head";
+import Image from "next/image";
+import styles from "../styles/Home.module.css";
 
 export default function Home() {
   return (
-   
-  )
+    <div>
+      <h1> Bienvenue sur mon portfolio</h1>
+      <p>
+        Logoden biniou degemer mat an penn ar bed, digalon ne levrioù ennon
+        anzav eta, mestr bed fresk gwinizh kerzu dreist.
+      </p>
+    </div>
+  );
 }
